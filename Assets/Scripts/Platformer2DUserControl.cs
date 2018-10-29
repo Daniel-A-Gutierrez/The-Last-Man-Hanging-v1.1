cc92b024f316320d3cb222c20bdb2505c89f2553
Daniel-A-Gutierrez <icanmakespaghetti@gmail.com>
2018-10-29 01:08:50 -0700

    total refactor, new prototye player controller

---
+++ Assets/Scripts/Platformer2DUserControl.cs
@@ -0,0 +1,137 @@
+using System;
+using UnityEngine;
+
+public class Platformer2DUserControl : MonoBehaviour
+{
+    private PlayerController2 m_Character;
+    private bool m_Jump;
+    int playerNumber;
+    public float hAim;
+    public float vAim; 
+    public float xMove;
+    public float yMove;
+    public bool LThrow;
+    public bool RThrow;
+    public bool jump;
+    Vector2 target;
+    Vector2 normalizedVelocityFactor;
+    Vector2 playerPosition;
+    public bool useItemContinuous;
+    public bool useItem1Frame;
+    public bool justPressed;
+    [SerializeField]
+    public bool controller;
+    public bool justJumped;
+    public bool boogie;
+    public bool inputing = true;
+    public Vector2 movedir;
+
+    private void Awake()
+    {
+        m_Character = GetComponent<PlayerController2>();
+        playerNumber = GetComponent<HardCodedGrapple>().PlayerNumber;
+        if (controller)
+        {
+            hAim = Input.GetAxis("Cont_" + playerNumber + "_Right_Horiz");
+            vAim = Input.GetAxis("Cont_" + playerNumber + "_Right_Vert");
+            xMove = Input.GetAxis("Cont_" + playerNumber + "_Left_Horiz");
+            yMove = Input.GetAxis("Cont_" + playerNumber + "_Left_Vert");
+            LThrow = Input.GetAxis("Cont_" + playerNumber + "_RB") > .95;
+            RThrow = Input.GetAxis("Cont_" + playerNumber + "_RT") > .95;
+            jump = Input.GetAxis("Cont_" + playerNumber + "_A") > .95;
+            useItemContinuous = Input.GetAxis("Cont_" + playerNumber + "_B") > .95;
+            useItem1Frame = Input.GetAxis("Cont_" + playerNumber + "_B") > .95;
+        }
+        else
+        {
+            target = Camera.main.ScreenToWorldPoint(Input.mousePosition); //thanks shawn for this function
+            playerPosition = transform.position;
+            normalizedVelocityFactor = new Vector2(target.x - playerPosition.x, target.y - playerPosition.y);
+            normalizedVelocityFactor.Normalize();
+            hAim = normalizedVelocityFactor.x;
+            vAim = -normalizedVelocityFactor.y;
+            xMove = Input.GetAxis("Horizontal");
+            yMove = Input.GetAxis("Vertical");
+            LThrow = Input.GetKey(KeyCode.Mouse0);
+            RThrow = Input.GetKey(KeyCode.Mouse1);
+            jump = Input.GetKeyDown(KeyCode.Space);
+            useItemContinuous = Input.GetKey(KeyCode.Return);
+            useItem1Frame = Input.GetKeyDown(KeyCode.Return);
+        }
+        movedir = new Vector2(xMove,yMove);
+    }
+
+
+    private void Update()
+    {
+        if (inputing)
+        {
+            if (controller)
+            {
+                hAim = Input.GetAxis("Cont_" + playerNumber + "_Right_Horiz");
+                vAim = Input.GetAxis("Cont_" + playerNumber + "_Right_Vert");
+                xMove = Input.GetAxis("Cont_" + playerNumber + "_Left_Horiz");
+                yMove = Input.GetAxis("Cont_" + playerNumber + "_Left_Vert");
+                LThrow = Input.GetAxis("Cont_" + playerNumber + "_RB") > .95;
+                RThrow = Input.GetAxis("Cont_" + playerNumber + "_RT") > .95;
+                jump = Input.GetKeyDown("joystick " + playerNumber + " button 8");
+                useItemContinuous = Input.GetAxis("Cont_" + playerNumber + "_B") > .95;
+                if (useItemContinuous & justPressed)
+                {
+                    useItem1Frame = false;
+                }
+                if (useItemContinuous & !justPressed)
+                {
+                    useItem1Frame = true;
+                    justPressed = true;
+                }
+
+                if (!useItemContinuous)
+                {
+                    justPressed = false;
+                    useItem1Frame = false;
+                }
+
+
+            }
+
+            else
+            {
+                target = Camera.main.ScreenToWorldPoint(Input.mousePosition); //thanks shawn for this function
+                playerPosition = transform.position;
+                normalizedVelocityFactor = new Vector2(target.x - playerPosition.x, target.y - playerPosition.y);
+                normalizedVelocityFactor.Normalize();
+                hAim = normalizedVelocityFactor.x;
+                vAim = -normalizedVelocityFactor.y;
+                xMove = Input.GetAxis("Horizontal");
+                yMove = Input.GetAxis("Vertical");
+                LThrow = Input.GetKey(KeyCode.Mouse0);
+                RThrow = Input.GetKey(KeyCode.Mouse1);
+                jump = Input.GetKeyDown(KeyCode.Space);
+                useItemContinuous = Input.GetKey(KeyCode.Return);
+                useItem1Frame = Input.GetKeyDown(KeyCode.Return);
+            }
+            movedir = new Vector2(xMove,yMove);
+        }
+    }
+    public void noInput()
+    {
+        inputing = false;
+    }
+    public void startInput()
+    {
+        inputing = true;
+    }
+
+
+    private void FixedUpdate()
+    {
+        // Read the inputs.
+        //bool crouch = Input.GetKey(KeyCode.LeftControl);
+        //float h = CrossPlatformInputManager.GetAxis("Horizontal");
+        // Pass all parameters to the character control script.
+        
+        //m_Jump = false;
+    }
+}
+
---
+++ Assets/Scripts/PlayerController2.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class PlayerController2 : MonoBehaviour 
+{
+	public int player_num;
+	public float walkSpeed = 10f;
+	public float jumpImpulse = 400f;
+	[Range(0,1)]
+	public float airControl = .5f; // the fraction  of ground accel that the player has in air
+	public LayerMask ground;
+	public float groundCheckOffset; 
+	public float groundCheckRadius; 
+	public float playerAccel;
+	bool facingRight;
+
+	private Platformer2DUserControl user;  
+	private Animator anim;
+	private Rigidbody2D rb;
+	private string State;
+	private Dictionary<string,Action> States;
+	
+	void Start () 
+	{
+		user = GetComponent<Platformer2DUserControl>(); 
+		anim = GetComponent<Animator>();
+		rb = GetComponent<Rigidbody2D>();
+		player_num = GetComponent<HardCodedGrapple>().PlayerNumber; //overrides player num in player controller
+		States = new Dictionary<string,Action> ();
+		State = "GROUNDED";
+		States["GROUNDED"] = GROUNDED;
+		States["AIRBORNE"] = AIRBORNE;
+		States["SWINGING"] = SWINGING;
+	}
+
+	void Update () 
+	{
+		States[State]();
+		setFace();
+	}
+
+	/*--------------------------------------------------------------------------------------------------------------------- */
+	void GROUNDED()
+	{
+		State = "GROUNDED";
+		if(!checkGround())
+		{
+			AIRBORNE();
+			return;
+		}
+		if(user.jump) 
+		{
+			rb.AddForce(new Vector2(0,1)*jumpImpulse,ForceMode2D.Impulse);
+			AIRBORNE();
+			return;
+		}
+		walk();
+	}
+
+	void AIRBORNE()
+	{
+		State = "AIRBORNE";
+		if(checkGround())
+		{
+			GROUNDED();
+			return;
+		}
+		fly();
+	}
+
+	void SWINGING()// currently not used, because swinging is handled independently by hard coded grapple. at best a flag can be obtained. 
+	{
+		State = "SWINGING";
+	}
+	/*--------------------------------------------------------------------------------------------------------------------- */
+
+	void walk()
+	{
+		//i want them to always have startup acceleration , but have no deceleration.
+		//sadly this is all i want to do for now.
+		rb.velocity = new Vector2(user.movedir.x*walkSpeed,rb.velocity.y);
+	}
+
+	void fly()
+	{
+		rb.velocity += new Vector2(user.movedir.x*airControl,rb.velocity.y);
+	}
+
+	void swing()
+	{
+
+	}
+
+	void setFace()
+	{
+		facingRight = (rb.velocity.x>0) ? true : false;
+		if(user.movedir.magnitude > .01f)
+		{
+			Vector3 theScale = transform.localScale;
+			theScale.x = new Vector2(rb.velocity.x,0).normalized.x;
+			transform.localScale = theScale;
+		}
+	}
+
+	bool checkGround()
+	{
+		Collider2D col = Physics2D.OverlapCircle(new Vector3(transform.position.x,transform.position.y-groundCheckOffset,0),groundCheckRadius,ground);
+		if(col != null)
+		{
+			return true;
+		}
+		return false;
+	}
+
+	/*----------------------------------------------------------------------------------------------------------------------- */
+	void OnDrawGizmosSelected()
+	{
+		Gizmos.color = Color.white;
+		Gizmos.DrawWireSphere(new Vector3(transform.position.x,transform.position.y-groundCheckOffset,0),groundCheckRadius);
+		//maybe one for jump height and grapple length as well? 
+	}
+
+	Vector3 v3(Vector2 v2)
+	{
+		return new Vector3(v2.x,v2.y,0);
+	}
+}
