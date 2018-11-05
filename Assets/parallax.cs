bfe7ab5daf2a2bab7914f0fc40350839725d55b9
Daniel Allen Gutierrez <icanmakespaghetti@gmail.com>
2018-11-05 13:28:18 -0800

    Parallax backup

---
+++ Assets/parallax.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class parallax : MonoBehaviour 
+{
+	Transform camTrans;
+	Vector3 oldCamPos;
+	Mesh m;
+	MeshFilter mf;
+	public float speed = .1f;
+
+	// Use this for initialization
+	void Start () 
+	{
+		camTrans = GameObject.FindGameObjectWithTag("MainCamera").transform;
+		oldCamPos = camTrans.position;
+		mf = GetComponent<MeshFilter>();
+		m = mf.mesh;
+		Vector2 [] uvs = m.uv;
+		foreach(Vector2 v in uvs)
+			print(v);
+	}
+	void Update () 
+	{
+		Vector3 offset = camTrans.position - oldCamPos;
+		oldCamPos = camTrans.position;
+		Vector2[] uvs = m.uv;
+
+		for(int i = 0; i<uvs.Length;i++)
+		{
+			uvs[i].x -= speed*offset.x/transform.localScale.x;
+			uvs[i].y -= speed*offset.y/transform.localScale.z;
+		}
+
+		m.uv = uvs;
+	}
+}
