37728fa473bf5793e718ab1e8bcfadc4a8d74100
hackathongang <44106252+hackathongang@users.noreply.github.com>
2018-11-05 09:55:58 -0800

    audiomanager now a prefab, complete

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -4,8 +4,19 @@ using System;
 
 public class AudioManager : MonoBehaviour {
     public Sound[] sounds;
+    public static AudioManager instance;
 	// Use this for initialization
 	void Awake () {
+        if (instance == null)
+            instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+
+        DontDestroyOnLoad(gameObject);
 		foreach (Sound s in sounds)
         {
             s.source = gameObject.AddComponent<AudioSource>();
@@ -13,12 +24,25 @@ public class AudioManager : MonoBehaviour {
 
             s.source.volume = s.volume;
             s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
         }
 	}
+
+    void Start()
+    {
+        Play("BackgroundMusic");
+    }
 
     public void Play(string name)
     {
         Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+
+        }
+             
         s.source.Play();
     }
 
