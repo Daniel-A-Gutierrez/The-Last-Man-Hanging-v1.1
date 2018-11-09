95a7edc008687a6b3fa12bc37a68fe50a6bf1a48
Wazyre <azalabduljalil@gmail.com>
2018-11-09 12:13:26 -0800

    Fix RandomLoadLevel

---
+++ Assets/Scripts/CursorChanger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorChanger : MonoBehaviour {
+
+	public Texture2D cursorTexture;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void OnMouseEnter () {
+		Cursor.SetCursor(cursorTexture);
+	}
+}
