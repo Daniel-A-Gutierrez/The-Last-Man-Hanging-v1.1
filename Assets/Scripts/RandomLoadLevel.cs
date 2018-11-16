34650987dd40bf30893e139dc48bcf5fd6e13d8f
Wazyre <azalabduljalil@gmail.com>
2018-11-16 12:35:01 -0800

    Fix Character Menu

--- Assets/Scripts/RandomLoadLevel.cs
+++ Assets/Scripts/RandomLoadLevel.cs
@@ -6,22 +6,27 @@ public class RandomLoadLevel : MonoBehaviour
 {
     //Platformer2DUserControl control;
     public bool sameLevel;
+    int numPlayers;
+    public static int playersLeft;
 
     void Start(){
-
+      numPlayers = playersLeft;
     }
     public void RandomLevel()
     {
         if (SceneManager.GetActiveScene().buildIndex >= 4){
-          GetComponent<CountdownStart>().playersLeft = GetComponent<CountdownStart>().numPlayers;
+          playersLeft = numPlayers;
+          Scene scene = SceneManager.GetActiveScene();
+          SceneManager.LoadScene(scene.buildIndex);
         }
         if (!sameLevel){
           int level = Random.Range(5, 8);
           SceneManager.LoadScene(level);
         }
-        else{
-          Scene scene = SceneManager.GetActiveScene();
-          SceneManager.LoadScene(scene.buildIndex);
-        }
+    }
+    public void PlayersLeft(int num)
+    {
+        playersLeft = num;
+        print(playersLeft);
     }
 }
