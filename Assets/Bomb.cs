1113f78387f72ec7a7526d9c04a63374ccc42946
Daniel Allen Gutierrez <icanmakespaghetti@gmail.com>
2018-11-19 16:06:01 -0800

    Bomb is Fully Implemented

--- Assets/Bomb.cs
+++ Assets/Bomb.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class Bomb : MonoBehaviour 
@@ -9,10 +10,22 @@ public class Bomb : MonoBehaviour
 	public float force;
 	float maxForce;//determines the maximum force that can be applied within the max Radius. 
 	public LayerMask trigger;
+	
+	public Animator animator;
+
+	public float TickTime;
+	public float tickSpeed;
+	public float idleSpeed;
+	public float explosionSpeed;
+	bool exploded = false;
+	AudioManager audioManager;
 
 	void Start () 
 	{
+		audioManager = GameObject.FindGameObjectWithTag("AudioManager").GetComponent<AudioManager>(); 
+		Array.Find(audioManager.sounds, sound => sound.name == "Phone_Dial_Beeps").source.pitch = tickSpeed/2;
 		maxForce = maxRadius/radius * force;
+		animator.speed = idleSpeed;//plays the idle by entry default.
 	}
 	
 	// Update is called once per frame
@@ -20,22 +33,51 @@ public class Bomb : MonoBehaviour
 	{
 		
 	}
-
+	//the animations have events which progress the method stream and terminate the explosion. 
 	void OnTriggerEnter2D(Collider2D collider)
 	{
-		if(collider.CompareTag("Hook"))
+		if(collider.CompareTag("Hook") & !exploded)
 		{
-			print("Boom");
-			
-			Collider2D[] players = Physics2D.OverlapCircleAll(transform.position,radius,trigger);
-			foreach(Collider2D col in players)
-			{
-				Vector3 offset = col.transform.position - transform.position;
-				float inverse = offset.magnitude > maxRadius ? 1/(offset.magnitude) : 1;
-				Vector2 offset2 = new Vector2(offset.x,offset.y).normalized;
-				col.gameObject.GetComponent<Rigidbody2D>().AddForce(offset2*force*inverse,ForceMode2D.Impulse);
-			}
-			gameObject.SetActive(false);
+			animator.Play("BombAnim");
+			audioManager.Play("Phone_Dial_Beeps");
+			animator.speed = tickSpeed;
+			exploded = true;
+			StartCoroutine("Tick");
 		}
 	}
+
+	void OnDrawGizmosSelected()
+	{
+		Gizmos.color = Color.white;
+		Gizmos.DrawWireSphere(transform.position, radius);
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireSphere(transform.position,maxRadius);
+	}
+
+	IEnumerator Tick() 
+	{
+		yield return new WaitForSeconds(TickTime);
+		EndTick();
+	}
+	void EndTick()
+	{
+		animator.Play("BombExplosion");
+		audioManager.Stop("Phone_Dial_Beeps");
+		audioManager.Play("explosion1");
+		animator.speed = explosionSpeed;
+		Collider2D[] players = Physics2D.OverlapCircleAll(transform.position,radius,trigger);
+		foreach(Collider2D col in players)
+		{
+			Vector3 offset = col.transform.position - transform.position;
+			float inverse = offset.magnitude > maxRadius ? (maxRadius/offset.magnitude) : 1;
+			Vector2 offset2 = new Vector2(offset.x,offset.y).normalized;
+			col.gameObject.GetComponent<Rigidbody2D>().AddForce(offset2*force*inverse,ForceMode2D.Impulse);
+		}
+	}
+
+	void EndExplosion()
+	{
+		gameObject.SetActive(false);
+	}
+	
 }
